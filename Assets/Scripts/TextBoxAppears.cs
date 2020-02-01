062c1456e01d63b45e2a27ad3c4845c556563f4f
ltejedor <lmtejedor@gmail.com>
2020-02-01 17:10:18 -0500

    add logic for chatbox appearing and disappearing

---
+++ Assets/Scripts/TextBoxAppears.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBoxAppears : MonoBehaviour
+{
+
+	public Canvas ChatCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//ChatCanvas = GetComponent<Canvas>();
+    	ChatCanvas.GetComponent<Canvas> ().planeDistance = 0;
+    }
+
+    void Update() {
+        if (Input.GetMouseButton(0))
+        {
+            ChatCanvas.GetComponent<Canvas> ().planeDistance = 100;
+        }
+    }
+}
