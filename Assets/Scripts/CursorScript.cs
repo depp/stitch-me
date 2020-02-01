c2bdfada27f347fcfc93349c5e056bf5a4949446
Jonathan Morales <jonmorales2.718@gmail.com>
2020-02-01 16:49:56 -0500

    saved the ToolBox and Cursor gameobjects to their respective prefabs,  added CursorScript.cs

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    public Sprite cursor;
+    private SpriteRenderer spriteRenderer;
+
+
+    private void Start()
+    {
+        spriteRenderer= GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            cursor = spriteRenderer.sprite;
+        }
+    }
+
+    public void Poop()
+    {
+
+    }
+}
