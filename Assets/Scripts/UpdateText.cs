165116078859a85393fe20d3274ddcf6f4c6eb9b
ltejedor <lmtejedor@gmail.com>
2020-02-01 16:41:34 -0500

    add a script to update text messages

---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UpdateText : MonoBehaviour
+{
+	public Text Msg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Msg = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    //should be changed to update on game events - pieces clicking together etc
+    void Update()
+    {
+    	Msg.text = "A new message";
+    }
+}
