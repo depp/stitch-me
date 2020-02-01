e2a92f6023335ba77f5fa2b101b16b22a5843d57
Dietrich Epp <depp@zdome.net>
2020-02-01 11:58:03 -0500

    Move parts when dragging

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IPointerDownHandler
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public void OnPointerDown(PointerEventData eventData)
+    // The drag anchor is the place on the object where the player clicked.
+    Vector2 dragAnchor;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        Vector2 pos = eventData.pressPosition;
-        Debug.LogFormat("PointerDown Pos: {0}", pos);
+        dragAnchor = transform.InverseTransformPoint(eventData.pointerPressRaycast.worldPosition);
+        UpdateDrag(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        UpdateDrag(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        UpdateDrag(eventData);
+    }
+
+    void UpdateDrag(PointerEventData eventData)
+    {
+        Vector2 currentAnchor = transform.TransformPoint(dragAnchor);
+        Vector2 targetAnchor = eventData.pointerCurrentRaycast.worldPosition;
+        Vector2 delta = targetAnchor - currentAnchor;
+        transform.position += (Vector3)delta;
     }
 
     // Start is called before the first frame update
