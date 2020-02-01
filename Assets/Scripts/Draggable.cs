02cc228e930722cf387e8ac646afcde5d2420422
Dietrich Epp <depp@zdome.net>
2020-02-01 11:38:11 -0500

    Detect clicks on bear parts

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 pos = eventData.pressPosition;
+        Debug.LogFormat("PointerDown Pos: {0}", pos);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
