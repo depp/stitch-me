d3003231f784965679cc82f4ffcea4d477521924
ltejedor <lmtejedor@gmail.com>
2020-02-01 21:00:58 -0500

    haircut scene 1 done

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class Clickable : MonoBehaviour, IPointerClickHandler
+{
+	[SerializeField]
+	private UnityEvent _response;
+
+	public void OnPointerClick(PointerEventData PointerEventData) {
+		_response.Invoke();
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
